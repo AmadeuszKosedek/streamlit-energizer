2ac583cf8e97d2a7db19f6688d49d2a23abf7c21
Amadeusz Kosędek <amadeusz.kosedek@gmail.com>
2023-06-28 11:48:09 +0200

    app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,24 @@
 import streamlit as st
-import yfinance as yf
 
-st.title("Stocks App")
-symbol = st.text_input("Enter a stock symbol", "AAPL")
-if st.button("Get Quote"):
-    st.json(yf.Ticker(symbol).info)
+from streamlit_gallery import apps, components
+from streamlit_gallery.utils.page import page_group
+
+def main():
+    page = page_group("p")
+
+    with st.sidebar:
+        st.title("Analiza śladu węglowego")
+
+        with st.expander("MOJA FIRMA", True):
+            page.item("Podstawowe informacje", apps.gallery, default=True)
+
+        with st.expander("ANALITYKA", True):
+            page.item("Zakres 1", components.ace_editor)
+            page.item("Zakres 2", components.disqus)
+            page.item("Zalres 3", components.elements)
+            page.item("Podsumowanie", components.pandas_profiling)
+    page.show()
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="Streamlit Gallery by Okld", page_icon="🎈", layout="wide")
+    main()
