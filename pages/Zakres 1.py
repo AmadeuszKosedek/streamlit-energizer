2b40f673f1d79eb5388a56366a159e36961e398e
Amadeusz Kosędek <amadeusz.kosedek@gmail.com>
2023-06-28 08:41:26 +0200

    karty

--- pages/Zakres 1.py
+++ pages/Zakres 1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Wgraj plik z danymi", type=['xls','xsls','csv'])
+uploaded_file = st.file_uploader("Wgraj plik z danymi", type=['xls','xlsx','csv'])
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
@@ -19,3 +19,17 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
+
+tab1, tab2, tab3 = st.tabs(["Stacjonarne źródła spalania", "Flota", "Pojazdy niedrogowe"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
